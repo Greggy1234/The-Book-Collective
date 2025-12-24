86b34c722333d9ba5c00fae2f7643acf413cf255
Greggy1234 <gregoreid@googlemail.com>
2025-12-24 15:25:14 +0000

    Start models for reviews app

--- book/models.py
+++ book/models.py
@@ -4,6 +4,9 @@ from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Genre(models.Model):
+    """
+    Stores the list of genres available for the Book class
+    """
     genre = models.CharField(max_length=75, unique=True)
 
     def __str__(self):
@@ -11,6 +14,9 @@ class Genre(models.Model):
 
 
 class Author(models.Model):
+    """
+    Stores the list of authors available for the Book class
+    """
     author = models.CharField(max_length=150, unique=True)
 
     def __str__(self):
@@ -18,6 +24,9 @@ class Author(models.Model):
 
 
 class Language(models.Model):
+    """
+    Stores the list of languages available for the Book class
+    """
     lang = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
@@ -25,6 +34,10 @@ class Language(models.Model):
 
 
 class Book(models.Model):
+    """
+    Stores a single book entry related to :model:`book.Genre`,
+    :model:`book.Author` and :model:`book.Language`
+    """
     title = models.CharField(max_length=250)
     first_published = models.DateField()
     pages = models.PositiveIntegerField(verbose_name="Number of Pages",
@@ -32,8 +45,14 @@ class Book(models.Model):
     author = models.ManyToManyField(Author)
     genres = models.ManyToManyField(Genre)
 #    #avg_rating
-    language = models.ForeignKey(Language, default="Language to be added",
+    language = models.ForeignKey(Language, default=1,
                                  on_delete=models.SET_DEFAULT)
     synopsis = models.TextField(blank=True)
 #    #front_cover
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return f'{self.title} by {self.author}, released in {self.first_published}'
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from book.models import Book
+
 
 # Create your models here.
+RATING_OPTIONS = ((0, 1), (1, 1.25), (2, 1.5), (3, 1.75),
+                  (4, 2), (5, 2.25), (6, 2.5), (7, 2.75),
+                  (8, 3), (9, 3.25), (10, 3.5), (11, 3.75),
+                  (12, 4), (13, 4.25), (14, 4.5), (15, 4.75), (16, 5))
+
+
+class Review(models.Model):
+    """
+    Stores a single instance of a review entry,
+    related to :model:`book.Book` and :model:`auth.User`
+    """
+    object = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="book_review"
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="review_author"
+    )
+    review = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f'Review of {self.object} by {self.author}'
+
+
+class Rating(models.Model):
+    """
+    Stores the numerical rating of a book by a user,
+    related to :model:`book.Book` and :model:`auth.User`
+    """
+    object = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="book_rating"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="rating_author"
+    )
+    rating = models.IntegerField(choices=RATING_OPTIONS, default=16)
