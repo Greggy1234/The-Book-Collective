0358b38e768ea50f43283ac74e9288f98a1d4b68
Greggy1234 <gregoreid@googlemail.com>
2026-01-08 13:41:25 +0000

    Add base code to profile.html and update status.views

--- status/views.py
+++ status/views.py
@@ -14,6 +14,10 @@ def profile_page(request, username):
     """
     user = get_object_or_404(User, username=username)
     book_status = Status.objects.select_related("object", "author").filter(author=user).order_by("-created_on")
+    book_status_wishlist = book_status.filter(status=1)[:4]
+    book_status_reading = book_status.filter(status=2)
+    book_status_read = book_status.filter(status=3)[:4]
+    book_status_dnf = book_status.filter(status=4)[:4]
     book_read_count = Status.objects.filter(author=user, status=3).count()
     user_avg_rating = Rating.objects.filter(author=user).aggregate(Avg('rating'))['rating__avg']
 
@@ -23,6 +27,10 @@ def profile_page(request, username):
         {
             "book_status": book_status,
             "book_read_count": book_read_count,
+            "book_status_wishlist": book_status_wishlist,
+            "book_status_reading": book_status_reading,
+            "book_status_read": book_status_read,
+            "book_read_count": book_status_dnf,
             "user": user,
             "avg_rating": user_avg_rating,
         }
