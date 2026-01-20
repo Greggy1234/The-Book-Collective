1988d24dcd3dc8c24b6aaf78f384d644c1e24f48
Greggy1234 <gregoreid@googlemail.com>
2026-01-20 20:01:45 +0000

    Add user stories and user story testing to read me

--- features/models.py
+++ features/models.py
@@ -27,3 +27,6 @@ class FutureFeaturesForm(models.Model):
     name = models.CharField(max_length=200)
     message = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.message}'
