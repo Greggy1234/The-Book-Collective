06d5d682fad9e7c6012745429d7835132be05b7d
Greggy1234 <gregoreid@googlemail.com>
2026-01-28 13:53:07 +0000

    Add new default resend email address and upadte password email txt file

--- book_collective/settings.py
+++ book_collective/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
 
@@ -179,5 +179,6 @@ else:
     EMAIL_HOST_USER = 'resend'
     EMAIL_HOST_PASSWORD = RESEND_API_KEY
 
-DEFAULT_FROM_EMAIL = 'onboarding@resend.dev'
+DEFAULT_FROM_EMAIL = 'book-collective@resend.dev'
 ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
+EMAIL_SUBJECT_PREFIX = ''
